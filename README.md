c324d461bc256477ef5983975dfa99465c5f4da3
sumin0407 <smpark@se.kaist.ac.kr>
2018-09-11 14:07:27 +0900

    test simulator

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Build Status](https://travis-ci.org/SESoS/SIMVA-SoS.svg?branch=master)](https://travis-ci.org/SESoS/SIMVA-SoS)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+# SIMVA-SoS?
+
+SIMVA-SoS is a simulation-based verification and analysis tool for system of systems. It simulates a system of systems, and performs statistical model checking to verify the achievement of SoS-level goals. SoS-level goals can be translated into properties of the SoS. It is an open framework consisting of independent modules: simulator, property checker and statistical hypothesiss test module.
+
+# More Information
+
+We are sorry to tell you that related publications are written in Korean. If you have any questions or suggesitions, please email us, [mgjin@se.kaist.ac.kr](mailto:mgjin@se.kaist.ac.kr). We are pleased to have talks with you.
+
+[Mingyu Jin, Donghwan Shin, Junho Kim, Doo-Hwan Bae. "System-of-Systems-level Goal Achievement Verification Tool." Proceedings of KIISE,  (2017.6): 552-554.](http://www.dbpia.co.kr/Journal/ArticleDetail/NODE07207302)
+
+[Junho Kim, Donghwan Shin, Doo-Hwan Bae. "An Applicability Study of Action-Benefit-Cost Model and Statistical Model Checking for System of Systems Goal Achievement Verification." KIISE Transactions on Computing Practices, 23.4 (2017.4): 256-261.](http://www.dbpia.co.kr/Journal/ArticleDetail/NODE07153954)
+
+
+
+## Copyright
+
+Copyright 2017 - SESoS Group in Korea Advanced Institute of Science and Technology (KAIST).
---
+++ src/main/java/simvasos/scenario/mciresponse/MCIResponseRunner.java
@@ -0,0 +1,131 @@
+package simvasos.scenario.mciresponse;
+
+import simvasos.simulation.Simulator;
+import simvasos.simulation.analysis.Snapshot;
+import simvasos.simulation.component.Scenario;
+import simvasos.scenario.mciresponse.MCIResponseScenario.SoSType;
+import simvasos.simulation.component.World;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+public class MCIResponseRunner {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleDateFormat datetimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        String testSession = "ABCPlus_AllTypes";
+
+
+        // 수정 --> Tick의 총 횟수
+        //int endTick = 7500; // 8000
+        int endTick = 1000;
+
+
+        int minTrial = 1;
+
+        // 수정 --> 각 케이스별로 돌리는 횟수
+        //int maxTrial = 100;
+        int maxTrial = 30;
+
+        try {
+            File simulationLogFile = new File(String.format("traces/" + testSession + "/" + testSession + "_simulation_logs.csv"));
+
+            BufferedWriter simulationLogWriter = new BufferedWriter(new FileWriter(simulationLogFile, true));
+
+//            simulationLogWriter.write("nPatient,nFireFighter,SoSType,Duration,MessageCount");
+//            simulationLogWriter.newLine();
+
+            // nPatient, nFireFighter
+
+            // 수정 (원본)
+            //SoSType[] targetTypeArray = {SoSType.Virtual, SoSType.Collaborative, SoSType.Acknowledged, SoSType.Directed};
+
+            SoSType[] targetTypeArray = {SoSType.Collaborative};
+
+            // 수정 (원본)
+            //int[] nPatientArray = {50, 100, 150, 200, 250};
+            //int[] nFireFighterArray = {2, 5, 10, 25, 50};
+
+            int[] nPatientArray = {10};
+            int[] nFireFighterArray = {4};
+
+            ArrayList<Snapshot> trace;
+            long startTime;
+            long duration;      // 매 케이스 별로 시스템 실행 시간
+            long durationSum;   // 각각의 케이스별 시스템 실행 시간의 총합
+            int messageCnt;
+            int messageCntSum;
+
+            for (int nPatient : nPatientArray) {
+                for (int nFireFighter : nFireFighterArray) {
+                    for (SoSType sostype : targetTypeArray) {
+                        System.out.println("Patient: " + nPatient + ", Firefighter: " + nFireFighter + ", SoS: " + sostype);
+                        System.out.println(datetimeFormat.format(new Date()));
+
+                        Scenario scenario = new MCIResponseScenario(sostype, nPatient, nFireFighter, 0, 10);
+                        World world = scenario.getWorld();
+
+                        durationSum = 0;
+                        messageCntSum = 0;
+                        world.setSeed(new Random().nextLong());
+                        for (int i = minTrial - 1; i <= maxTrial; i++) {
+                            //world.setSeed(new Random().nextLong());
+                            ((MCIResponseWorld) world).setSoSType(sostype);
+
+                            startTime = System.currentTimeMillis();
+
+                            trace = Simulator.execute(world, endTick);      // 여기서 들어가서 fault를 넣어야 할듯. 아니면 message쪽까지 가서 해야하나?
+
+                            if (i == minTrial - 1)
+                                continue;
+
+                            duration = (System.currentTimeMillis() - startTime);
+                            durationSum += duration;
+                            messageCnt = (int) world.getCurrentSnapshot().getProperties().get(1).value;
+                            messageCntSum += messageCnt;
+
+                            simulationLogWriter.write(nPatient + "," + nFireFighter + "," + sostype.toString() + "," + duration + "," + messageCnt);
+                            simulationLogWriter.newLine();
+
+                            writeTrace(trace, String.format("traces/%s/%04d_%03d_%s_%04d.txt", testSession, nPatient, nFireFighter, sostype, i));
+                        }
+
+                        simulationLogWriter.flush();
+                        System.out.println("Average duration: " + durationSum / (maxTrial - minTrial + 1));
+                        System.out.println("Average messageCnt: " + messageCntSum / (maxTrial - minTrial + 1));
+                    }
+                }
+            }
+
+            simulationLogWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Session complete: " + datetimeFormat.format(new Date()));
+    }
+
+    private static void writeTrace(List<Snapshot> trace, String filename) {
+        try {
+            File file = new File(filename);
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+            for (Snapshot snapshot : trace) {
+                bw.write((snapshot.getProperties().get(0).value).toString());
+                bw.newLine();
+            }
+
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
