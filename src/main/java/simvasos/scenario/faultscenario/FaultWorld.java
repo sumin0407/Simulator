50efc059021e16bc7b73b7e37ee11dea0db0d527
Unknown <psumin0407@gmail.com>
2018-09-12 15:45:09 +0900

    FaultWorld 복구.....
    깃을 잘못 사용해서 날라간 FaultWorld 코드 복구

--- src/main/java/simvasos/scenario/faultscenario/FaultWorld.java
+++ src/main/java/simvasos/scenario/faultscenario/FaultWorld.java
@@ -1,4 +1,73 @@
 package simvasos.scenario.faultscenario;
 
-public class FaultWorld {
+import simvasos.scenario.mciresponse.MCIResponseScenario.SoSType;
+import simvasos.scenario.mciresponse.MCIResponseWorld;
+import simvasos.simulation.component.Message;
+
+import java.util.*;
+
+public class FaultWorld extends MCIResponseWorld {
+
+    private class DelayedMessage {
+        Message msg;
+        int delay;
+
+        public DelayedMessage(Message msg, int delay) {
+            this.msg = msg;
+            this.delay = delay;
+        }
+    }
+
+    private ArrayList<DelayedMessage> delayedMessages = new ArrayList<DelayedMessage>();
+
+    public FaultWorld(SoSType type, int nPatient) {
+        super(type, nPatient);
+    }
+
+    @Override
+    public void reset() {
+        delayedMessages.clear();
+        super.reset();
+    }
+
+    @Override
+    public void progress(int time) {
+
+        assert time == 1 : "현재 이 함수는 time이 1이라는 전제를 바탕으로 작성되었음";
+
+        // 삭제 대기열과 관련한 코드는 성능을 개선시킬 여지가 있다고 생각됨.
+
+        // 삭제 대기열
+        ArrayList<DelayedMessage> mustRemoveMsgs = new ArrayList<DelayedMessage>();
+        for(DelayedMessage delayedMsg : delayedMessages) {
+            if(delayedMsg.delay == 0) {
+                // 삭제 대기열에 추가
+                mustRemoveMsgs.add(delayedMsg);
+
+                super.sendMessage(delayedMsg.msg);
+            } else {
+                delayedMsg.delay--;
+            }
+        }
+
+        // 삭제 대기열에 넣어둔 delayedMsg 들을 delayedMessages에서 제거
+        for(DelayedMessage mustRemoveMsg : mustRemoveMsgs) {
+            delayedMessages.remove(mustRemoveMsg);
+        }
+
+        super.progress(time);
+    }
+
+    @Override
+    public void sendMessage(Message msg) {
+        // 특정 조건(ex. 확률)을 만족 시키는 경우, delay를 걸어줌
+        boolean isDelay = false;
+        if(isDelay) {
+            // 딜레이 몇 초?
+            int delay = 3;
+            delayedMessages.add(new DelayedMessage(msg, delay));
+        } else {
+            super.sendMessage(msg);
+        }
+    }
 }
